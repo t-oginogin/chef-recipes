8aa4618da55593309d70c4c53831143d96a50eb5
Tatsuya Ogi <t.oginogin@gmail.com>
2013-10-09 07:13:30 +0900

    add recipe to install rvm

---
+++ cookbooks/rvm/attributes/default.rb
@@ -0,0 +1,1 @@
+default['rvm']['ruby_version']    = '2.0.0'
---
+++ cookbooks/rvm/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: rvm
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+ruby_version     = node['rvm']['ruby_version']
+
+bash 'install_rvm' do
+  not_if "test -e ~/.rvm"
+  user 'vagrant'
+  group 'vagrant'
+
+  code <<-EOL
+    curl -L https://get.rvm.io | bash -s stable
+    source ~/.bash_profile
+  EOL
+end
+
+bash 'install_ruby' do
+  not_if "test -e ~/.rvm/rubies/ruby-#{ruby_version}*"
+  user 'vagrant'
+  group 'vagrant'
+
+  code <<-EOL
+    source ~/.bash_profile
+    rvm install #{ruby_version}
+    rvm use #{ruby_version} --default
+  EOL
+end
