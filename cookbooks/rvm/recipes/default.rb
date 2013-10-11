a8f349eb886a935743eb25e10f86a3d43a1f1b81
Tatsuya Ogi <t.oginogin@gmail.com>
2013-10-12 06:54:23 +0900

    add recipe to install rails

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -31,3 +31,23 @@ bash 'install_ruby' do
     rvm use #{ruby_version} --default
   EOL
 end
+
+bash 'set ruby version' do
+  user 'vagrant'
+  group 'vagrant'
+  code <<-EOL
+    source ~/.bash_profile
+    rvm use #{ruby_version} --default
+  EOL
+end
+
+gem_package 'bundler' do
+  gem_binary "~/.rvm/bin/gem"
+  action :upgrade
+end
+
+gem_package 'rails' do
+  gem_binary "~/.rvm/bin/gem"
+  action :upgrade
+end
+
