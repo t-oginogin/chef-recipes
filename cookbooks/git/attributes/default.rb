8e68ce1827a0018ebbbb431a49a6195c20ba4a51
Tatsuya Ogi <t.oginogin@gmail.com>
2013-10-16 08:37:28 +0900

    add --with-curl option to git configure

--- cookbooks/git/attributes/default.rb
+++ cookbooks/git/attributes/default.rb
@@ -1,4 +1,5 @@
+default['git']['curl_version']    = '7.33.0'
+default['git']['curl_source_uri'] = 'http://curl.haxx.se/download/curl-7.33.0.tar.bz2'
 default['git']['version']    = '1.8.4'
 default['git']['source_uri'] = 'https://git-core.googlecode.com/files/git-1.8.4.tar.gz'
-default['git']['configure']  = './configure'
 default['git']['packages']   = %w{gettext gettext-devel zlib-devel openssl-devel}
--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -8,9 +8,10 @@
 #
 
 install_dir = '/usr/local/src'
+curl_version     = node['git']['curl_version']
+curl_source_uri  = node['git']['curl_source_uri']
 version     = node['git']['version']
 source_uri  = node['git']['source_uri']
-configure   = node['git']['configure']
 
 node['git']['packages'].each do |package_name|
   package "#{package_name}" do
@@ -18,8 +19,27 @@ node['git']['packages'].each do |package_name|
   end
 end
 
-remote_file "/tmp/git-#{version}.tar.gz" do
-  not_if 'which git'
+remote_file "#{Chef::Config[:file_cache_path]}/curl-#{curl_version}.tar.bz2" do
+  not_if "test -e #{Chef::Config[:file_cache_path]}/curl-#{curl_version}.tar.bz2"
+  source "#{curl_source_uri}"
+end
+
+bash 'install_curl' do
+  not_if 'test -e /usr/local/curl'
+  user 'root'
+
+  code <<-EOL
+    install -d #{install_dir}
+    tar xf #{Chef::Config[:file_cache_path]}/curl-#{curl_version}.tar.bz2 -C #{install_dir}
+    cd #{install_dir}/curl-#{curl_version}
+    ./configure --prefix=/usr/local/curl
+    make && make install && make clean
+    alias curl='/usr/local/curl/bin/curl'
+  EOL
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/git-#{version}.tar.gz" do
+  not_if "test -e #{Chef::Config[:file_cache_path]}/git-#{version}.tar.gz"
   source "#{source_uri}"
 end
 
@@ -29,9 +49,9 @@ bash 'install_git' do
 
   code <<-EOL
     install -d #{install_dir}
-    tar xfz /tmp/git-#{version}.tar.gz -C #{install_dir}
-    rm -rf /tmp/git-#{version}.tar.gz
+    tar xfz #{Chef::Config[:file_cache_path]}/git-#{version}.tar.gz -C #{install_dir}
     cd #{install_dir}/git-#{version}
-    #{configure} && make && make install && make clean
+    ./configure --with-curl=/usr/local/curl
+    make && make install && make clean
   EOL
 end
